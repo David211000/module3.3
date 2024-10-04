b4ae0a8a3533865368663bd7029111040657d0fe
David211000 <dsrapyan@gmail.com>
2024-10-04 10:54:51 +0300

    Add files via upload

--- module3.3.py
+++ module3.3.py
@@ -1,5 +1,6 @@
 def print_params(a = 1, b = 'строка', c = True):
     print(a,b,c)
+print_params()
 print_params(b=25,c=[1,2,3])
 values_list=[4,'str',False]
 values_dict={'a':3,'b':3.12,'c':'string'}
